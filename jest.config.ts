0c8f88cd49cc2dbc29625121550d56dcf394ff70
sgash708 <sugash7081023@gmail.com>
2021-12-15 18:16:20 +0900

    ts導入

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  }
+};
