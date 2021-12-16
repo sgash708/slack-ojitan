e83a3f86741950e0b6ec7a0c6050eab5dd610176
sgash708 <sugash7081023@gmail.com>
2021-12-16 15:01:33 +0900

    不要な出力を削除する

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,4 @@ const app: express.Express = express();
 const port: string = process.env.PORT || '3000';
 
 app.get('/', (req, res) => res.send('Hello world!!'));
-console.log("hoge");
 app.listen(port, () => console.log(`listening on port ${port}!`));
